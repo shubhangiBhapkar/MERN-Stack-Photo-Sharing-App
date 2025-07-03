d630d34701b20752671b8d7ab43b8bcd72814bc4
web Development <shubhangibhapkar35@gmail.com>
2025-07-03 22:10:15 +0530

    DB Connection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import imageRoute from "./Routes/ImageRoutes.js";
-
+import mongoose from "mongoose";
 //const port=4000;
 const app=express();
 
@@ -11,6 +11,16 @@ const app=express();
 app.use(morgan("dev"));
 dotenv.config();
 app.use(cors());
+
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    console.log("MongoDB connection established");
+  })
+  .catch(err => {
+    console.log("Connection error:", err);
+  });
+
 
 app.get("/",(req,res)=>{
     res.json("working");
